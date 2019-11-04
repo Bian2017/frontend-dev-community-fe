5dbda9873c05926fbc64377b84051a7c25777d6e
Bian2017 <bgl_cumt_zju@163.com>
2019-11-04 09:59:59 +0800

    feat: 搭建登录模块的工程化环境和添加验证码功能

---
+++ examples/login/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action>
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input
+              type="password"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+          <div class="layui-form-mid" v-html="captcha"></div>
+        </div>
+        <button type="button" class="layui-btn">点击登录</button>
+        <a class="forget-password" href="http://www.layui.com">忘记密码</a>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      captcha: '',
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:3001/getCaptcha').then(res => {
+      console.log('res:', res)
+      if (res.status === 200) {
+        const resData = res.data
+        if (resData.code === 0) {
+          this.captcha = resData.data
+        }
+      }
+    })
+  }
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+  background: #f2f2f2;
+}
+
+.layui-container {
+  background: #fff;
+}
+
+input {
+  width: 200px;
+}
+
+.forget-password {
+  margin-left: 20px;
+
+  &:hover {
+    color: #009688;
+  }
+}
+</style>
