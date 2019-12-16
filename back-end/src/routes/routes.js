099e3e09a2105c00eaa99acd5c8bb7d6f5f52202
Bian2017 <bgl_cumt_zju@163.com>
2019-12-16 09:27:39 +0800

    feat: 删除后端项目，已单独迁移成库

--- back-end/src/routes/routes.js
+++
@@ -1,5 +0,0 @@
-import combineRoutes from 'koa-combine-routers'; // 拼接路由
-import publicRouter from './publicRouter';
-import loginRouter from './loginRouter';
-
-export default combineRoutes(publicRouter, loginRouter);
